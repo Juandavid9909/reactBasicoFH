0fcde60cdb77b653fa2e1bc0f4ce5ee8fc1fde59
Juan David <juandavid918@gmail.com>
2024-03-03 12:50:23 -0500

    Bases javascript

---
+++ 02-intro-js/src/bases/05-funciones.js
@@ -0,0 +1,13 @@
+// Tarea
+// 1. Transformen a una función de flecha
+// 2. Tiene que retornar un objeto implícito
+// 3. Pruebas
+const getUsuarioActivo = (nombre) => {
+    return {
+        uid: "ABC567",
+        username: nomber
+    }
+}
+
+const usuarioActivo = getUsuarioActivo("Fernando");
+console.log(usuarioActivo);
