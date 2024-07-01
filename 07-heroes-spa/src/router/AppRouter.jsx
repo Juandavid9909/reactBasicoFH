466adf18b4452a1a3e3b32a4f66d04de26829757
Juan David <juandavid918@gmail.com>
2024-07-01 16:33:10 -0500

    Diseños casi completos

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,19 +1,14 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import { DcPage } from "../heroes/pages/DcPage";
-import { LoginPage } from "../auth/pages/LoginPage";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { HeroesRoutes } from "../heroes";
+import { LoginPage } from "../auth";
 
 export const AppRouter = () => {
     return (
         <Routes>
-            <Route path="marvel" element={ <MarvelPage /> } />
-
-            <Route path="dc" element={ <DcPage /> } />
-
             <Route path="login" element={ <LoginPage /> } />
 
-            <Route path="/" element={ <Navigate to="/marvel" /> } />
+            <Route path="/*" element={ <HeroesRoutes /> } />
         </Routes>
     );
 }
