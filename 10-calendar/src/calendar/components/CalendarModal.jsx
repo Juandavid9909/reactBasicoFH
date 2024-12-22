4558af7dc6478755933cdd0767f9128cc074b4ca
Juan David <juandavid918@gmail.com>
2024-12-22 16:13:53 -0500

    Componente de calendario con validaciones

---
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -0,0 +1,170 @@
+import { addHours, differenceInSeconds } from "date-fns";
+import { es } from "date-fns/locale/es";
+import { useState } from "react";
+import DatePicker, { registerLocale } from "react-datepicker";
+import Modal from "react-modal";
+import Swal from "sweetalert2";
+
+import "react-datepicker/dist/react-datepicker.css";
+import "sweetalert2/dist/sweetalert2.min.css";
+import { useMemo } from "react";
+
+registerLocale("es", es);
+
+const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+};
+
+Modal.setAppElement("#root");
+
+export const CalendarModal = () => {
+    const [isOpen, setIsOpen] = useState(true);
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const [formValues, setFormValues] = useState({
+        title: "Juan",
+        notes: "David",
+        start: new Date(),
+        end: addHours(new Date(), 2)
+    });
+
+    const titleClass = useMemo(() => {
+        if(!formSubmitted) return "";
+
+        return formValues.title.length > 0
+            ? ""
+            : "is-invalid";
+    }, [formValues.title, formSubmitted]);
+
+    const onInputChanged = ({ target }) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value
+        });
+    };
+
+    const onDateChanged = (event, changing) => {
+        setFormValues({
+            ...formValues,
+            [changing]: event
+        });
+    };
+
+    const onCloseModal = () => {
+        console.log("Cerrando modal");
+        setIsOpen(false);
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setFormSubmitted(true);
+
+        const difference = differenceInSeconds(formValues.end, formValues.start);
+
+        if(isNaN(difference) || difference <= 0) {
+            Swal.fire("Fechas incorrectas", "Revisar las fechas ingresadas", "error");
+
+            return;
+        }
+
+        if(formValues.title.length <= 0) return;
+    };
+
+    return (
+        <Modal
+            className="modal"
+            closeTimeoutMS={ 200 }
+            isOpen={ isOpen }
+            onRequestClose={ onCloseModal }
+            overlayClassName="modal-fondo"
+            style={ customStyles }
+        >
+            <h1> Nuevo evento </h1>
+
+            <hr />
+
+            <form className="container" onSubmit={ onSubmit }>
+                <div className="form-group mb-2">
+                    <label>Fecha y hora inicio</label>
+
+                    <div className="customDatePickerWidth">
+                        <DatePicker
+                            className="form-control"
+                            dateFormat="Pp"
+                            locale="es"
+                            onChange={ (event) => onDateChanged(event, "start") }
+                            selected={ formValues.start }
+                            showTimeSelect
+                            timeCaption="Hora"
+                        />
+                    </div>
+                </div>
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora fin</label>
+
+                    <div className="customDatePickerWidth">
+                        <DatePicker
+                            className="form-control"
+                            dateFormat="Pp"
+                            locale="es"
+                            minDate={ formValues.start }
+                            onChange={ (event) => onDateChanged(event, "end") }
+                            selected={ formValues.end }
+                            showTimeSelect
+                            timeCaption="Hora"
+                        />
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="form-group mb-2">
+                    <label>Titulo y notas</label>
+
+                    <input 
+                        autoComplete="off"
+                        className={ `form-control ${ titleClass }` }
+                        name="title"
+                        onChange={ onInputChanged }
+                        placeholder="Título del evento"
+                        type="text" 
+                        value={ formValues.title }
+                    />
+
+                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                </div>
+
+                <div className="form-group mb-2">
+                    <textarea 
+                        className="form-control"
+                        name="notes"
+                        onChange={ onInputChanged }
+                        placeholder="Notas"
+                        rows="5"
+                        type="text"
+                        value={ formValues.notes }
+                    ></textarea>
+
+                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                </div>
+
+                <button
+                    className="btn btn-outline-primary btn-block"
+                    type="submit"
+                >
+                    <i className="far fa-save"></i>
+
+                    <span> Guardar</span>
+                </button>
+            </form>
+        </Modal>
+    );
+};
--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,5 +1,77 @@
+import { addHours } from "date-fns";
+import { Calendar } from "react-big-calendar";
+import { useState } from "react";
+
+import { CalendarEvent, CalendarModal, Navbar } from "../";
+import { getMessagesEs, localizer } from "../../helpers";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const events = [
+    {
+        title: "Cumpleaños del jefe",
+        notes: "Hay que comprar el pastel",
+        start: new Date(),
+        end: addHours(new Date(), 2),
+        bgColor: "#FAFAFA",
+        user: {
+            _id: "123",
+            name: "Juan"
+        }
+    }
+];
+
 export const CalendarPage = () => {
+    const [lastView, setLastView] =  useState(localStorage.getItem("lastView") || "week");
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: "#347CF7",
+            borderRadius: "0px",
+            opacity: 0.8,
+            color: "white"
+        };
+
+        return {
+            style
+        };
+    };
+
+    const onDoubleClick = (event) => {
+
+    };
+
+    const onSelect = (event) => {
+        
+    };
+
+    const onViewChanged = (event) => {
+        localStorage.setItem("lastView", event);
+    };
+
     return (
-        <div>CalendarPage</div>
+        <>
+            <Navbar />
+
+            <Calendar
+                components={{
+                    event: CalendarEvent
+                }}
+                culture="es"
+                defaultView={ lastView }
+                endAccessor="end"
+                eventPropGetter={ eventStyleGetter }
+                events={ events }
+                localizer={ localizer }
+                messages={ getMessagesEs() }
+                onDoubleClickEvent={ onDoubleClick }
+                onSelectEvent={ onSelect }
+                onView={ onViewChanged }
+                startAccessor="start"
+                style={{ height: "calc(100vh - 80px)" }}
+            />
+
+            <CalendarModal />
+        </>
     );
 };
