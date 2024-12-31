603b4af389dc1765a1ed8bc0e70b7c5cc08d1950
Juan David <juandavid918@gmail.com>
2024-12-31 14:36:18 -0500

    Base del backend de app Calendar finalizada

--- 11-calendar-backend/routes/auth.js
+++ 11-calendar-backend/routes/auth.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 
 const { crearUsuario, loginUsuario, revalidarToken } = require("../controllers/auth");
 const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
@@ -19,6 +20,6 @@ router.post("/", [
     validarCampos
 ], loginUsuario);
 
-router.get("/renew", revalidarToken);
+router.get("/renew", validarJWT, revalidarToken);
 
 module.exports = router;
