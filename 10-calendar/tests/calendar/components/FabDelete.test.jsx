d1e38d68c02ea3a561b044a3e67747bca02893fa
Juan David <juandavid918@gmail.com>
2025-02-08 14:09:29 -0500

    Pruebas en proceso

---
+++ 10-calendar/tests/calendar/components/FabDelete.test.jsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+
+import { FabDelete } from "../../../src/calendar/components/FabDelete";
+import { Provider } from "react-redux";
+import { store } from "../../../src/store";
+
+describe("Pruebas en <FabDelete />", () => {
+    test("debe de mostrar el componente correctamente", () => {
+        render(
+            <Provider store={ store }>
+                <FabDelete />
+            </Provider>
+        );
+    });
+});
---
+++ 10-calendar/tests/fixtures/calendarStates.js
@@ -0,0 +1,33 @@
+export const events = [{
+        id: "1",
+        start: new Date("2022-10-21 13:00:00"),
+        end: new Date("2022-10-21 15:00:00"),
+        title: "Cumpleaños de Juan",
+        notes: "Alguna nota"
+    },
+    {
+        id: "2",
+        start: new Date("2022-11-09 13:00:00"),
+        end: new Date("2022-11-09 15:00:00"),
+        title: "Cumpleaños de Karen",
+        notes: "Alguna nota de Karen"
+    }
+];
+
+export const initialState = {
+    isLoadingEvents: true,
+    events: [],
+    activeEvent: null
+};
+
+export const calendarWithEventsState = {
+    isLoadingEvents: false,
+    events: [...events],
+    activeEvent: null
+};
+
+export const calendarWithActiveEventState = {
+    isLoadingEvents: false,
+    events: [...events],
+    activeEvent: {...events[0] }
+};
